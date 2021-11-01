b2046346c336e3d729715454d745821e6b3d26d3
tiffany-dumaire <44428702+tiffany-dumaire@users.noreply.github.com>
2021-11-01 12:45:59 +0100

    add sidebar

--- src/features/fichestechniques/ListeFichesTechniquesPage.tsx
+++ src/features/fichestechniques/ListeFichesTechniquesPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { SidebarMenu } from '../../layout/sidebar-menu/SidebarMenu';
 import styles from './ListeFichesTechniquesPage.module.css';
 
 export function ListeFichesTechniquesPage(): JSX.Element {
@@ -9,6 +10,20 @@ export function ListeFichesTechniquesPage(): JSX.Element {
                 <title>{'Liste des fiches techniques'}</title>
             </Helmet>
             <div className={styles.listeFTContainer}>
+                <SidebarMenu 
+                    width={300} 
+                    height={'600px'} 
+                    to={
+                        [
+                            {to: '', name: 'Créer une fiche technique'},
+                            {to: '/fiches techniques', name: 'Rechercher une fiche technique'},
+                            {to: '', name: 'Imprimer une fiche technique'},
+                            {to: '', name: 'Créer une étape'},
+                            {to: '', name: 'Ajouter un ingrédient au mercurial'},
+                            {to: '/liste des allergenes', name: 'Voir la liste des allergènes'},
+                        ]
+                    }
+                />
                 <input placeholder="rechercher une fiche technique"></input>
             </div>
         </>
---
+++ src/layout/sidebar-menu/SideBarButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from './SideBarButton.module.css';
+
+export type SideBarButtonProps = {
+    to: string;
+    name: string;
+};
+
+export const SideBarButton: React.FunctionComponent<SideBarButtonProps> = (props: SideBarButtonProps) => {
+  return(
+    <NavLink exact to={`${props.to}`} className={styles.navButton}>
+      <div>
+        <p>{props.name[0].toUpperCase() + props.name.slice(1)}</p>
+      </div>
+    </NavLink>
+  );
+}
